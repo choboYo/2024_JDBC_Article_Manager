62296d08730873e2935189e38b2c0884552c3ba5
choboYo <tjddlsmn96@gmail.com>
2024-05-22 21:30:49 +0900

    작업 7, article modify 진행중

--- src/com/koreaIT/JAM/dto/Article.java
+++ src/com/koreaIT/JAM/dto/Article.java
@@ -14,5 +14,21 @@ public class Article {
 		this.title = title;
 		this.body = body;
 	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
 
 }
