73188e6f683ac4edec2e70b50136e7b241226060
choboYo <tjddlsmn96@gmail.com>
2024-05-24 22:46:38 +0900

    작업 14, Controller 적용

--- src/com/koreaIT/JAM/dto/Article.java
+++ src/com/koreaIT/JAM/dto/Article.java
@@ -1,19 +1,20 @@
 package com.koreaIT.JAM.dto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class Article {
 	public int id;
-	public LocalDateTime regDate;
-	public LocalDateTime updateDate;
+	public String regDate;
+	public String updateDate;
 	public String title;
 	public String body;
 	
 	public Article(Map<String, Object> articleMap) {
 		this.id = (int) articleMap.get("id");
-		this.regDate = (LocalDateTime) articleMap.get("regDate");
-		this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+		this.regDate = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format( (LocalDateTime)articleMap.get("regDate"));
+		this.updateDate = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format( (LocalDateTime)articleMap.get("updateDate"));;
 		this.title = (String) articleMap.get("title");
 		this.body = (String) articleMap.get("body");
 	}
